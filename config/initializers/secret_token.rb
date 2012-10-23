5fe4c21dabb2739b9b5cd3246b8f5de4d563e5aa
Ana Chicote Villar <aniii@iota07.aulas.gsyc.es>
2012-10-23 12:38:58 +0200

    Versión lista para subir a github y a heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myrottenpotatoes::Application.config.secret_token = '15e29f7f5bfc8afd362a72253cef641a9aadb693a323d5b7f4b551e3a40d66a843232c29b5da0eff992fd7e2ee9c12280bfbd74c595ffbad5cb873fc62d7bbfb'
